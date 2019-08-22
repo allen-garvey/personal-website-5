bf9f9f3d77322b326bfc8f80d0830b582d35147e
Allen Garvey <allengarvey@hotmail.com>
2019-08-21 23:21:52 -0400

    Edit site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Allen Garvey | Full-stack Developer`,
+    description: `Personal website for Allen Garvey, full-stack software developer`,
+    author: `Allen Garvey`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
